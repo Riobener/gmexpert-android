665214c9ec71d2d7ac1cd1f41a8ba7f6a7e735ba
Riobener <venisonsheff@gmail.com>
2022-06-05 23:34:52 +0300

    Hilt setting up

--- app/src/main/java/com/riobener/gamesexpert/MainActivity.kt
+++ app/src/main/java/com/riobener/gamesexpert/MainActivity.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import com.riobener.gamesexpert.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val mBinding get() = _binding!!
---
+++ app/src/main/java/com/riobener/gamesexpert/di/GameExpertApp.kt
@@ -0,0 +1,7 @@
+package com.riobener.gamesexpert.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GameExpertApp: Application()
