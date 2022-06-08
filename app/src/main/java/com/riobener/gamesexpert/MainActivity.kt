020ea488be98c9eb0da028ef7bb90f43d6a5d31a
Riobener <venisonsheff@gmail.com>
2022-06-08 20:15:12 +0300

    Added side menu

--- app/src/main/java/com/riobener/gamesexpert/MainActivity.kt
+++ app/src/main/java/com/riobener/gamesexpert/MainActivity.kt
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.Gravity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.riobener.gamesexpert.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -15,15 +22,22 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val mBinding get() = _binding!!
-
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_splash)
+
         CoroutineScope(Dispatchers.Main).launch {
             delay(1500)
             _binding = ActivityMainBinding.inflate(layoutInflater)
             setContentView(mBinding.root)
+            val controller = Navigation.findNavController(login_host.requireView())
+            mBinding.navView.setupWithNavController(controller)
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return Navigation.findNavController(mBinding.root.rootView).navigateUp()|| super.onSupportNavigateUp()
     }
 
     override fun onDestroy() {
